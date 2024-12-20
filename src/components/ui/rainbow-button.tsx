3f0f9f70080d9bebda10e4b070efb7c100e53042
denizbinay <deniz@bombig.net>
2024-12-20 02:31:07 +0100

    Fix: Adjust RainbowButton props to resolve build issue

--- src/components/ui/rainbow-button.tsx
+++ src/components/ui/rainbow-button.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-
 import { cn } from "@/lib/utils";
-interface RainbowButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export function RainbowButton({
   children,
   className,
   ...props
-}: RainbowButtonProps) {
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       className={cn(
-        "group relative inline-flex h-11 animate-rainbow cursor-pointer items-center justify-center rounded-xl border-0 bg-[length:200%] px-8 py-2 font-medium text-zinc-50 transition-colors [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:text-zinc-900 dark:focus-visible:ring-zinc-300",
+        "inline-flex relative justify-center items-center border-0 bg-[length:200%] disabled:opacity-50 px-8 py-2 [border:calc(0.08*1rem)_solid_transparent] [background-clip:padding-box,border-box,border-box] [background-origin:border-box] rounded-xl focus-visible:ring-1 focus-visible:ring-zinc-950 dark:focus-visible:ring-zinc-300 h-11 font-medium text-zinc-50 dark:text-zinc-900 transition-colors animate-rainbow cursor-pointer disabled:pointer-events-none focus-visible:outline-none group",
 
         // before styles
         "before:absolute before:bottom-[-20%] before:left-1/2 before:z-0 before:h-1/5 before:w-3/5 before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))] before:[filter:blur(calc(0.8*1rem))]",
